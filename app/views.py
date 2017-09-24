cf7f33a5448d8c86355e1fc142ad39bef86199c5
georgenavarro1802 <gnavarrog1802@gmail.com>
2017-09-24 01:29:44 -0400

    Media folder for church web

--- app/views.py
+++ app/views.py
@@ -4,12 +4,9 @@ from hopch.settings import MEDIA_URL, MEDIA_ROOT, DEBUG
 
 
 def index(request):
-    data = {'title': 'Guerreros de Cristo'}
-    if DEBUG:
-        data['domain_url'] = "{}{}".format('http://', request.get_host())
-    else:
-        if 'www' not in request.get_host():
-            data['domain_url'] = "{}{}".format('https://www.', request.get_host())
-
-    data['media_url'] = MEDIA_URL
-    return render(request, "index.html", data)
+    return render(request, "index.html",
+                  {
+                      'title': 'Guerreros de Cristo',
+                      'domain_url': "{}{}".format('http://', request.get_host()),
+                      'media_url': MEDIA_URL
+                  })
